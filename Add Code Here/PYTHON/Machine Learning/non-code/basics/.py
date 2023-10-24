cb1e263f11c30f9e02b8933640c29f3136e6b499
VanshikaNagarajan <137884653+VanshikaNagarajan@users.noreply.github.com>
2023-10-25 00:08:53 +0530

    Create .py

---
+++ Add Code Here/PYTHON/Machine Learning/non-code/basics/.py
@@ -0,0 +1,48 @@
+Introduction:
+Machine Learning is a rapidly evolving field within artificial intelligence. Whether it's a speech recognition system, breast cancer prediction, image recognition system, or recommendation system for e-commerce websites, machine learning is playing a pivotal role.
+
+What is Machine Learning?
+Machine Learning is all about teaching computers to make predictions and decisions based on data. Instead manually, the machine itself discovers the trends and relationships among the data.
+
+Just the way we teach children to differentiate between apples and oranges with the help of labelled images.
+
+Types of Machine Learning:
+Supervised Learning - This type of learning, uses labelled datasets i.e; it has both output and input data. It connects the input data to the output data and makes predictions for future new datasets.
+
+Unsupervised Learning - This is quite interesting to know because this type of machine learning algorithm trains itself and finds trends and relationships, given an unlabelled dataset with the help of clustering method.
+
+Semi-Supervised Learning - This machine learning method uses a small set of labelled datasets and a large set of unlabelled datasets. It is a combination of supervised and unsupervsied learning.
+
+Reinforcement Learning - Reinforcement algorithm is unique because, it interacts with the environment and learns to make decisions to maximise a reward.
+
+Keywords to know:
+Training - This means to 'teach', it refers to the learning process.
+
+Validation - Refers to validate the learnings, by getting the testing dataset tested with the help of the trained model. Followed by finding the accuracy score to check the model's prediction accuracy.
+
+Testing - Comes in place to test the real data when deployed.
+
+For example, your lectures are your training session, internal exams are for professors to validate their training if you are performing well and then comes your testing part as your semester examination.
+
+Overfitting - When the training dataset learns too much because, then it starts learning from the noise and inaccurate data.
+
+Underfitting - There isn't enough data in the training dataset. Underfitting occurs when a complex model tries to learn from a simple dataset.
+
+Clustering - Grouping of data which is similar in a group.
+
+Challenges in Machine Learning:
+Data quality is a major challenge in machine learning because accuracy depends upon data quality. Followed by algorithmic bias which may produce unfair outcomes. For example, a facial recognition system may learn to recognise white person more than black person.
+
+Applications:
+Healthcare - disease diagnosis, patient monitoring and so on
+
+Finance - fraud detection, stock market predictions, banking sector
+
+Computer vision - facial recognition system, person/object detection, self driving cars
+
+e-commerce - recommendation system
+
+NLP - speech recognition, language learning and translations, chatbots, sentiment analysis.
+
+Conclusion
+- Machine learning is a dynamic and exciting field that has the potential to transform various industries and our daily lives. While it may seem daunting at first, with the right resources and dedication, anyone can get started in machine learning.
